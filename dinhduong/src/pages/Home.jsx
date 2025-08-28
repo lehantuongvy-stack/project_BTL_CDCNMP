d193dba7c61a86b2bca4b61419875b18ea90804e
ThuQuynh182 <tongthuquynh182@gmail.com>
2025-08-28 11:25:16 +0700

    Trang chu

---
+++ dinhduong/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home-content">
+        <h2>Chào mừng đến với DinhDuong Shop</h2>
+        <p>Khám phá sản phẩm dinh dưỡng chất lượng cao</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
