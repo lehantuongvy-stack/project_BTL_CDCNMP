aa972c45c0b2795bf336eaba65b4f3bd019d1c19
Hoàng Huyền <hhuyen9104@gmail.com>
2025-09-06 08:39:28 +0700

    https://github.com/lehantuongvy-stack/project_BTL_CDCNMP.git

---
+++ giaodienkho/src/assets/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WarehouseForm from './WarehouseForm';
+
+function App() {
+  return (
+    <div className="App">
+      <WarehouseForm />
+    </div>
+  );
+}
+
+export default App;
